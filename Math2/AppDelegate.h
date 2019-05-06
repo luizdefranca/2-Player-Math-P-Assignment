c64cee5e49028fc8ec51cfcf2a3a6564f39365c8
LuizRamos <luizramospe@hotmail.com>
2019-05-06 12:49:57 -0400

    First Commit

---
+++ Math2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Math2
+//
+//  Created by Luiz on 5/6/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Math2Tests/Math2Tests.m
@@ -0,0 +1,37 @@
+//
+//  Math2Tests.m
+//  Math2Tests
+//
+//  Created by Luiz on 5/6/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Math2Tests : XCTestCase
+
+@end
+
+@implementation Math2Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
